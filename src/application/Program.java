fdb620c1746682f466c0b4f7d926b9df17cdd9b3
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-02-20 09:24:11 +0000

    FindById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,12 @@
-import java.sql.Date;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
   public static void main(String[] args) {
-    Department obj = new Department(1, "Books");
-
-    Seller seller = new Seller(
-      21,
-      "Bob",
-      "bob@gmail.com",
-      new Date(25 / 02 / 2023),
-      3000.0,
-      obj
-    );
     SellerDao sellerDao = DaoFactory.creatSellerDao();
+    Seller seller = sellerDao.findById(3);
 
     System.out.println(seller);
   }
